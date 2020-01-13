e8307395f856dba1abc03d1d6de578dda560584d
justJackjon <github@redrobincreative.com>
2020-01-13 10:15:10 +0000

    Add default button styling

--- src/Components/Main/Cards/ArticleCard/ArticleCard.js
+++ src/Components/Main/Cards/ArticleCard/ArticleCard.js
@@ -6,6 +6,16 @@ const ArticleCard = () => {
     <article className="article-card">
       <h3>ARTICLE CARD</h3>
       <Button className="btn-sm btn-solid">BUTTON</Button>
+      <Button className="btn-md btn-solid">BUTTON</Button>
+      <Button className="btn-lg btn-solid">BUTTON</Button>
+      <br />
+      <Button className="btn-sm btn-regular">BUTTON</Button>
+      <Button className="btn-md btn-regular">BUTTON</Button>
+      <Button className="btn-lg btn-regular">BUTTON</Button>
+      <br />
+      <Button className="btn-sm btn-secondary">BUTTON</Button>
+      <Button className="btn-md btn-secondary">BUTTON</Button>
+      <Button className="btn-lg btn-secondary">BUTTON</Button>
     </article>
   );
 };
