6cd2f604c37b9423c1935ea50b10bdf776cc3636
justJackjon <github@redrobincreative.com>
2020-01-29 21:00:28 +0000

    Refactor ErrorPage

--- src/Components/Main/Pages/ErrorPage.js
+++ src/Components/Main/Pages/ErrorPage.js
@@ -1,24 +1,9 @@
 import React from 'react';
 
-import { WindowConsumer } from '../../Context/WindowProvider';
-import SideBar from '../SharedComponents/SideBar/SideBar';
-import MainContainer from '../Containers/MainContainer/MainContainer';
 import MessageCard from '../Cards/MessageCard/MessageCard';
 
-const ErrorPage = ({ users }) => {
-  return (
-    <>
-      <MainContainer>
-        <WindowConsumer>
-          {({ windowWidth }) => {
-            const showSideBar = windowWidth > 1024;
-            return showSideBar && <SideBar users={users} />;
-          }}
-        </WindowConsumer>
-        <MessageCard icon="thumbs-down" title="404" message="Page not found." />
-      </MainContainer>
-    </>
-  );
-};
+const ErrorPage = () => (
+  <MessageCard icon="thumbs-down" title="404" message="Page not found." />
+);
 
 export default ErrorPage;
