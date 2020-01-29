c11656ab2f511517e84529dfa7f8e0960aa3a7ba
justJackjon <github@redrobincreative.com>
2020-01-29 08:10:33 +0000

    Localise state to hold topicArticles lower down in component tree

--- src/Components/Main/Containers/TopicContainer/TopicContainer.js
+++ src/Components/Main/Containers/TopicContainer/TopicContainer.js
@@ -1,88 +1,10 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import * as api from '../../../../api';
-import Loader from '../../../Utils/Loader/Loader';
+import React from 'react';
 import '../Containers.css';
 
-const TopicContainer = ({
-  getAddtlData,
-  topicArticles,
-  updateMainState,
-  parent: { topic },
-  path,
-  sort_by,
-  currentSort,
-  children
-}) => {
-  // const getAddtl = useCallback(() => {
-  //   getAddtlData();
-  // }, [getAddtlData]);
-
-  // useEffect(() => {
-  //     window.addEventListener('scroll', getAddtl);
-  //     return () => {
-  //       window.removeEventListener('scroll', getAddtl);
-  //     };
-  // }, [getAddtl, path]);
-  const [displayLoader, setDisplayLoader] = useState(false);
-
-  const fetchTopicArticles = useCallback(() => {
-    setDisplayLoader(true);
-    api
-      .getData('/articles', 'articles', { params: { topic, sort_by } })
-      .then(topicArticles => {
-        if (topicArticles.length) {
-          updateMainState({ topicArticles, currentSort: sort_by });
-        } else {
-          throw new Error('no data');
-        }
-        setDisplayLoader(false);
-      })
-      .catch(error => {
-        if (error.message === 'no data') {
-          updateMainState({
-            topicArticles: [
-              {
-                article_id: `../topics/${topic}`,
-                title: `No articles for '${topic}'`,
-                body: error.msg,
-                votes: null,
-                topic: topic,
-                author: 'the server',
-                created_at: new Date().toISOString(),
-                comment_count: 'No'
-              }
-            ],
-            currentSort: sort_by
-          });
-        } else {
-          updateMainState({ error: error.response });
-        }
-      });
-  }, [sort_by, topic, updateMainState]);
-
-  useEffect(() => {
-    if (topicArticles[0]?.title === `No articles for '${topic}'`) return;
-    if (!topicArticles.length) fetchTopicArticles();
-    if (topicArticles.length) {
-      if (topicArticles.every(article => article.topic !== topic))
-        fetchTopicArticles();
-    }
-    if (sort_by !== currentSort) fetchTopicArticles();
-  }, [currentSort, fetchTopicArticles, sort_by, topic, topicArticles]);
-
-  return (
-    <>
-      {displayLoader ? (
-        <Loader className="loading">
-          <h1>LOADING JUICY ARTICLES...</h1>
-        </Loader>
-      ) : (
-        <div className="main-content">
-          <div className="main-content-container">{children}</div>
-        </div>
-      )}
-    </>
-  );
-};
+const TopicContainer = ({ children }) => (
+  <div className="main-content">
+    <div className="main-content-container">{children}</div>
+  </div>
+);
 
 export default TopicContainer;
--- src/Components/Main/Pages/TopicPage.js
+++ src/Components/Main/Pages/TopicPage.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
+import * as api from '../../../api';
 import { WindowConsumer } from '../../Context/WindowProvider';
+
+import Loader from '../../Utils/Loader/Loader';
 import SubHeader from '../SubHeader/SubHeader';
 import SideBar from '../SharedComponents/SideBar/SideBar';
 import TopicContainer from '../Containers/TopicContainer/TopicContainer';
@@ -10,51 +13,106 @@ const TopicPage = props => {
   const {
     path,
     uri,
+    topic,
     sort_by,
     currentSort,
     updateMainState,
-    // getAddtlData,
     topics,
     users,
-    topicArticles,
+    // getAddtlData,
     loadAddtlData,
     dataAvailable
   } = props;
 
+  const [displayLoader, setDisplayLoader] = useState(false);
+  const [topicArticles, setTopicArticles] = useState([]);
+
+  const fetchTopicArticles = useCallback(() => {
+    setDisplayLoader(true);
+    api
+      .getData('/articles', 'articles', { params: { topic, sort_by } })
+      .then(topicArticles => {
+        if (topicArticles.length) {
+          setTopicArticles(topicArticles);
+          updateMainState({ currentSort: sort_by });
+        } else {
+          throw new Error('no data');
+        }
+        setDisplayLoader(false);
+      })
+      .catch(error => {
+        if (error.message === 'no data') {
+          setTopicArticles([
+            {
+              article_id: `../topics/${topic}`,
+              title: `No articles for '${topic}'`,
+              body: error.msg,
+              votes: null,
+              topic: topic,
+              author: 'the server',
+              created_at: new Date().toISOString(),
+              comment_count: 'No'
+            }
+          ]);
+          setDisplayLoader(false);
+          updateMainState({ currentSort: sort_by });
+        } else {
+          updateMainState({ error: error.response });
+        }
+      });
+  }, [sort_by, topic, updateMainState]);
+
+  useEffect(() => {
+    if (topicArticles[0]?.title === `No articles for '${topic}'`) return;
+    if (!topicArticles.length) fetchTopicArticles();
+    if (topicArticles.length) {
+      if (topicArticles.every(article => article.topic !== topic))
+        fetchTopicArticles();
+    }
+    if (sort_by !== currentSort) fetchTopicArticles();
+  }, [currentSort, fetchTopicArticles, sort_by, topic, topicArticles]);
+
   return (
     <>
-      <SubHeader
-        path={path}
-        uri={uri}
-        sort_by={sort_by}
-        updateMainState={updateMainState}
-      />
-      <TopicContainer
-        parent={props}
-        updateMainState={updateMainState}
-        topicArticles={topicArticles}
-        path={path}
-        sort_by={sort_by}
-        currentSort={currentSort}
-      >
-        <WindowConsumer>
-          {({ windowWidth }) => {
-            const showSideBar = windowWidth > 1024;
-            return showSideBar && <SideBar users={users} />;
-          }}
-        </WindowConsumer>
-        <Feed
-          sort_by={sort_by}
-          path={path}
-          topics={topics}
-          users={users}
-          updateMainState={updateMainState}
-          dataType="articles"
-          articles={topicArticles}
-          loadAddtlData={loadAddtlData}
-          dataAvailable={dataAvailable}
-        />
-      </TopicContainer>
+      {displayLoader ? (
+        <Loader className="loading">
+          <h1>LOADING JUICY ARTICLES...</h1>
+        </Loader>
+      ) : (
+        <>
+          <SubHeader
+            path={path}
+            uri={uri}
+            sort_by={sort_by}
+            updateMainState={updateMainState}
+          />
+          <TopicContainer
+            parent={props}
+            updateMainState={updateMainState}
+            path={path}
+            sort_by={sort_by}
+            currentSort={currentSort}
+          >
+            <WindowConsumer>
+              {({ windowWidth }) => {
+                const showSideBar = windowWidth > 1024;
+                return showSideBar && <SideBar users={users} />;
+              }}
+            </WindowConsumer>
+            <Feed
+              sort_by={sort_by}
+              path={path}
+              topics={topics}
+              users={users}
+              updateMainState={updateMainState}
+              dataType="articles"
+              articles={topicArticles}
+              loadAddtlData={loadAddtlData}
+              dataAvailable={dataAvailable}
+            />
+          </TopicContainer>
+        </>
+      )}
     </>
   );
 };
