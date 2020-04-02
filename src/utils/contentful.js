78f2b56662ef3e9d346ca29ffd5ddb5cafd9201e
justJackjon <jackjon@redrobincreative.com>
2020-04-02 14:48:22 +0100

    test commit

---
+++ src/utils/contentful.js
@@ -0,0 +1,8 @@
+import contentful from 'contentful';
+
+const client = contentful.createClient({
+  space: 'dibsthaa31ai',
+  accessToken: 'I1M2-p9vC8VvrqSUR9OB7gIA_iLa6atlRF7RvoIEoEk'
+});
+
+const previewAccessToken = 'LFS-MUHN3luTCXmXaJ_f3wfHNsFJt5TLGItWq2Jy3b0';
