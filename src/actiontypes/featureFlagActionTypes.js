5a95b02c62028af73d00b0841843b6b556e0bcd8
justJackjon <jackjon@redrobincreative.com>
2020-04-05 17:10:32 +0100

    feature(split.io): enable feature flag actions in redux

---
+++ src/actiontypes/featureFlagActionTypes.js
@@ -0,0 +1,3 @@
+export const SET_FLAGS_CLIENT = 'featureFlag/SET_FLAGS_CLIENT';
+export const SET_FLAGS_READY = 'featureFlag/SET_FLAGS_READY';
+export const SET_FLAGS_TIMED_OUT = 'featureFlag/SET_FLAGS_TIMED_OUT';
