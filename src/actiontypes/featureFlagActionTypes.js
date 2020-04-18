3c00d40051762c2237131c21f03318fbc669a147
justJackjon <jackjon@redrobincreative.com>
2020-04-18 19:44:32 +0100

    chore(splitio): delete deprecated files no longer required for legacy SDK

--- src/actiontypes/featureFlagActionTypes.js
+++
@@ -1,3 +0,0 @@
-export const SET_FLAGS_CLIENT = 'featureFlag/SET_FLAGS_CLIENT';
-export const SET_FLAGS_READY = 'featureFlag/SET_FLAGS_READY';
-export const SET_FLAGS_TIMED_OUT = 'featureFlag/SET_FLAGS_TIMED_OUT';
